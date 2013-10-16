15a48170f26a392ec0732c80a70f7b1e386d8249
Guilherme Baufaker Rego <gbre.111@gmail.com>
2013-10-16 17:43:09 -0300

    Estrutura do PHP adaptada

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Teste</title>
+        <script type="text/javascript" src="maps.js">
+        
+        </script>>
+    </head>
+    <body>
+    </body>
+</html>
