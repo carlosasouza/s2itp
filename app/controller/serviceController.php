7c0054929caf84b8425c4b93df4e1bf68e9bf3d0
Jônatas Medeiros de Mendonça <jonatasmm@gmail.com>
2013-10-23 18:04:17 -0200

    Criado uma função que recupera dados do onibus do banco de dados.

--- app/controller/serviceController.php
+++ app/controller/serviceController.php
@@ -17,6 +17,20 @@ class ServiceController {
         
         return "Foi!";
     }
+   //em desenvolvimento 
+    static function getData() {
+        
+        $pdo = Connection::getConnection();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+        
+        $stmt = $pdo->prepare("SELECT `latitude`,`longitude` FROM `bus` WHERE `idBus` = 1");
+        //$stmt->bindParam(1, $latitude, PDO::PARAM_STR);
+        //$stmt->bindParam(2, $longitude, PDO::PARAM_STR);
+        //$stmt->bindParam(3, $busId, PDO::PARAM_STR);
+        $result = $stmt->execute();
+        
+        return $result;
+    }
 }
 
 ?>
