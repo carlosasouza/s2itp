ee7c7393761f481f16aaa31c5500647192a931d6
MaylonFelix <maylon.felix@gmail.com>
2013-10-25 18:25:18 -0200

    Coloca um marker na posição do ônibus.

--- app/controller/serviceController.php
+++ app/controller/serviceController.php
@@ -4,7 +4,7 @@ include_once 'DataBase/dataBase.php';
 
 class ServiceController {
     
-    static function saveData($latitude, $longitude, $busId) {
+    static function saveData($latitude, $longitude, $idBus) {
         
         $pdo = Connection::getConnection();
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
@@ -12,28 +12,38 @@ class ServiceController {
         $stmt = $pdo->prepare("UPDATE `bus` SET `latitude`=?,`longitude`=? WHERE `idBus` = ?");
         $stmt->bindParam(1, $latitude, PDO::PARAM_STR);
         $stmt->bindParam(2, $longitude, PDO::PARAM_STR);
-        $stmt->bindParam(3, $busId, PDO::PARAM_STR);
+        $stmt->bindParam(3, $idBus, PDO::PARAM_STR);
         $result = $stmt->execute();
         
         return "Foi!";
     }
     
     
-    function getBusInfo() {
+    function getBusInfo($idBus) {
   
         $pdo = Connection::getConnection();
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
   
+        $stmt = $pdo->prepare("SELECT `idBus`, `latitude`,`longitude` FROM `bus` WHERE `idBus` = ?");
+        $stmt->bindParam(1, $idBus, PDO::PARAM_STR);
+        $result = $stmt->execute();
+        $ok = $stmt->fetchAll();
+        
+        if(is_array($ok))
+            return $ok;
+        
+        else return false;
         
         
-        $sql = "SELECT `idBus`, `latitude`,`longitude` FROM `bus` WHERE `idBus` = 1";
-        foreach ($pdo->query($sql) as $row) {
+               
+        /*foreach ($pdo->query($sql) as $row) {
         print "idBus:". $row['idBus'] . "\t";
         print "latitude:".$row['latitude'] . "\t";
         print "longitude:".$row['longitude'] . "\n";
         
-        }
         
+        }
+        return $pdo->query($sql);*/
         }
 }
 
