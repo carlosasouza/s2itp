75c2ae837b948c73921c1e6e4c0c0300ea29cdba
Carlos André de Souza <carlos.andre.souza@msn.com>
2013-12-08 18:18:31 -0200

    Refatoração do nome do arquivo e titulo do painel.

---
+++ public_html/painel_informativo.php
@@ -0,0 +1,133 @@
+﻿<!DOCTYPE html>
+<html>
+    <head>
+
+        <!-- start: Meta -->
+        <meta charset="utf-8">
+        <title>Painel de Informações</title>
+        <meta name="description" content="Painel de Informações.">
+        <meta name="keyword" content="Painel de Informações">
+        <!-- end: Meta -->
+
+        <!-- start: Mobile Specific -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- end: Mobile Specific -->
+
+        <!-- start: CSS -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/css/style.min.css" rel="stylesheet">
+        <link href="assets/css/retina.min.css" rel="stylesheet">
+        <!-- end: CSS -->
+
+
+        <!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+        
+                <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+                <script src="assets/js/respond.min.js"></script>
+                <script src="assets/css/ie6-8.css"></script>
+                
+        <![endif]-->
+
+        <!-- start: Favicon and Touch Icons -->
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+        <!-- end: Favicon and Touch Icons -->	
+
+    </head>
+
+    <body>
+
+        <div class="col-lg-12">
+            <div class="box">
+                <div class="box-header">
+                    <h2><i class="icon-align-justify"></i><span class="break"></span>Parada 345 - Zoologico</h2>
+                    <div class="box-icon">
+                        <h2>10:50</h2>
+
+                    </div>
+                </div>
+                <div class="box-content">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Linha</th>
+                                <th>Destino</th>
+                                <th>Tempo de Chegada</th>
+
+                            </tr>
+                        </thead>   
+                        <tbody>
+                            <tr>
+                                <td><span class="label label-success">200.1</span></td>
+                                <td class="center">Rodoviária do Plano Piloto</td>
+                                <td class="center">10 minutos</td>
+
+                            </tr>
+                            <tr>
+                                <td>200.1</td>
+                                <td class="center">Rodoviária do Plano Piloto</td>
+                                <td class="center">10 minutos</td>
+
+                            </tr>
+                            <tr>
+                                <td>200.1</td>
+                                <td class="center">Rodoviária do Plano Piloto</td>
+                                <td class="center">10 minutos</td>
+
+                            </tr>
+                            <tr>
+                                <td>200.1</td>
+                                <td class="center">Rodoviária do Plano Piloto</td>
+                                <td class="center">10 minutos</td>
+
+                            </tr>
+                            <tr>
+                                <td>200.1</td>
+                                <td class="center">Rodoviária do Plano Piloto</td>
+                                <td class="center">10 minutos</td>
+
+                            </tr>                   
+
+                            <tr>
+                                <td><span class="label label-success">200.1</span></td>
+                                <td class="center">Rodoviária do Plano Piloto</td>
+                                <td class="center">10 minutos</td>
+
+                            </tr>
+                            <tr>
+                                <td>200.1</td>
+                                <td class="center">Rodoviária do Plano Piloto</td>
+                                <td class="center">10 minutos</td>
+
+                            </tr>
+                            <tr>
+                                <td>200.1</td>
+                                <td class="center">Rodoviária do Plano Piloto</td>
+                                <td class="center">10 minutos</td>
+
+                            </tr>
+                            <tr>
+                                <td>200.1</td>
+                                <td class="center">Rodoviária do Plano Piloto</td>
+                                <td class="center">10 minutos</td>
+
+                            </tr>
+                            <tr>
+                                <td>200.1</td>
+                                <td class="center">Rodoviária do Plano Piloto</td>
+                                <td class="center">10 minutos</td>
+
+                            </tr>                 
+                        </tbody>
+                    </table>  
+
+                </div>
+            </div>
+        </div><!--/col-->
+    </div><!--/row-->
+
+</body>
+
+</html>
