2f226fa592446854dc148bc21d2187a6914239bf
Carlos André de Souza <carlos.andre.souza@msn.com>
2013-12-08 12:45:17 -0200

    Inclusão da index.php na index raíz do projeto.

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Teste</title>
-        <script type="text/javascript" src="maps.js">
-        
-        </script>>
-    </head>
-    <body>
-    </body>
-</html>
+<?php
+
+include_once './public_html/index.php';
+
+?>
